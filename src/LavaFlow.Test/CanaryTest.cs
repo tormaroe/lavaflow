928d5e2fdce4cb57572b3695679918f2e3ebec9b
Torbjørn Marø <torbjorn.maro@gmail.com>
2015-05-16 12:30:39 +0200

    LavaFlow proj, LavaFlow.Test proj, dependencies.

---
+++ src/LavaFlow.Test/CanaryTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace LavaFlow.Test
+{
+    [TestFixture]
+    public class CanaryTest
+    {
+        [Test]
+        public void PipPip()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
